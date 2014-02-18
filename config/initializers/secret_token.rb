ae6fa12bdf4b7e85242a1bbc60a43372c125e39e
Digital-Learner <digital.learning.journeys@gmail.com>
2014-02-18 09:49:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogParsing::Application.config.secret_token = 'd684a69d02461ab33df7f9d0039389b5c8e5c38a40739608136404106f65f81907bb8f7c495e7643f5eabaf723d37cbbc5378fd526c21d4901001fb4d1664b7e'
